4ac23d24a3d5227b82f93fabb4564e48773833fa
sa <halil@DESKTOP-TSHLOIG>
2023-06-04 11:05:57 +0300

    Add project files.

---
+++ Miniminimini men/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miniminimini_men
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] isimler = { "Ali", "Veli", "Ali", "Ahmet", "Mehmet", "Ali", "Zehra", "Fatma", "Ayşe", "Ali" };
+
+            Console.WriteLine(Array.IndexOf(args, isimler));
+
+            Console.WriteLine(isimler.Length);
+            Array.IndexOf(args, isimler);
+
+            int İndex = Convert.ToInt32(Console.ReadLine());
+
+            foreach (var item in isimler)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
